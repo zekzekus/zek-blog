af65844eddba6ea642281ff22a03479fd1595ae7
Zekeriya KOC <zekus@zetbook.(none)>
2009-10-31 16:41:49 +0200

    some translation fixes.

--- layout.lisp
+++ layout.lisp
@@ -6,11 +6,11 @@
 	  :initform (get-instances-by-class 'blog-post))))
 
 (defmethod ucw::render ((self summary))
-  (<:h1 "Summary of posts")
+  (<:h1 "All posts")
   (dolist (post (summary.posts self))
     (<:h2 (<:as-html (blog-post.title post)))
     (<:p (<:as-html (blog-post.body post)))
-    (<:as-html "Yorum: " (comment-count post)) (<:br)
+    (<:as-html "Comments: " (comment-count post)) (<:br)
     (<ucw:a :action (post-detail self post) "Detail")
     (<:hr)))
 
@@ -25,7 +25,7 @@
   (<:a :href "/blog/index.zek" "<<Home")
   (<:h1 (<:as-html (blog-post.title (detail.post self))))
   (<:p (<:as-html (blog-post.body (detail.post self))))
-  (<:h3 "Yorumlar:")
+  (<:h3 "Comments:")
   (dolist (comment (comments-as-list (detail.post self)))
     (<:strong (<:as-html (comment.email comment)))
     (<:p (<:as-html (comment.body comment)))
